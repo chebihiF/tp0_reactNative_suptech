ab8bb785dbdddec9e973803a009c7ff5e60a5fc9
chebihiF <chebihi.f@ucd.ac.ma>
2022-04-15 15:09:08 +0000

    add Button

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import {
   StyleSheet, 
   Text,
   Image, 
+  Button,
   View,
+  Alert,
   SafeAreaView,
   Platform,
-  StatusBar
+  StatusBar,
+  TouchableWithoutFeedback,
+  TouchableOpacity
  } from 'react-native';
+
 
 export default function App() {
   return (
@@ -19,14 +24,24 @@ export default function App() {
             but the title provides its own additional styles. 
             The title and body will stack on top of each other on 
             account of the literal newlines</Text>
+      <TouchableOpacity  onPress={()=>console.log("Image tapped")}>
+        <Image 
+          source={{
+          uri: "https://picsum.photos/200",
+          width: 200,
+          height: 200
+        }} />
+      </TouchableOpacity>
       <Image style={styles.myImage} source={require('./assets/favicon.png')} /> 
-      <Image 
-        blurRadius={2}
-        source={{
-        uri: "https://picsum.photos/200",
-        width: 200,
-        height: 200
-      }} />
+      <Button 
+        title='Click me' 
+        color='orange' 
+        onPress={()=>Alert.alert('My title', 'My Message', [
+          {text: 'YES', onPress: ()=>console.log("Yes Pressed")},
+          {text: 'No', onPress: ()=>console.log("No Pressed")}
+        ])} />
+
+
     </SafeAreaView>
   );
 }
